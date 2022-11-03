c09c3fec4d3edaa51167efd58a49457b35e0e4ae
Aldino Pratama <aldinopratam15@gmail.com>
2022-11-03 21:09:05 +0700

    create endpoint role

---
+++ endpoints/role.go
@@ -0,0 +1,54 @@
+package endpoints
+
+import (
+	"github.com/bagasunix/ginclean/domains"
+	"github.com/bagasunix/ginclean/endpoints/requests"
+	"github.com/gin-gonic/gin"
+)
+
+type RoleEndpoint interface {
+	CreateRole() gin.HandlerFunc
+	ListRole() gin.HandlerFunc
+	ViewRole() gin.HandlerFunc
+	UpdateRole() gin.HandlerFunc
+	DeleteRole() gin.HandlerFunc
+}
+
+type roleHandler struct {
+	service domains.Service
+}
+
+// CreateRole implements RoleEndpoint
+func (r *roleHandler) CreateRole() gin.HandlerFunc {
+	return func(g *gin.Context) {
+		var req requests.CreateRole
+		if err := g.Bind(&req); err != nil {
+			return
+		}
+		r.service.CreateRole(g, &req)
+	}
+}
+
+// DeleteRole implements RoleEndpoint
+func (*roleHandler) DeleteRole() gin.HandlerFunc {
+	panic("unimplemented")
+}
+
+// ListRole implements RoleEndpoint
+func (*roleHandler) ListRole() gin.HandlerFunc {
+	panic("unimplemented")
+}
+
+// UpdateRole implements RoleEndpoint
+func (*roleHandler) UpdateRole() gin.HandlerFunc {
+	panic("unimplemented")
+}
+
+// ViewRole implements RoleEndpoint
+func (*roleHandler) ViewRole() gin.HandlerFunc {
+	panic("unimplemented")
+}
+
+func NewRoleEndpoint(svc domains.Service) RoleEndpoint {
+	return &roleHandler{service: svc}
+}
