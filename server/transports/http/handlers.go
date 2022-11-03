22eb310ff92f2f64e42f2d3bb501a933bd0ac624
Aldino Pratama <aldinopratam15@gmail.com>
2022-11-04 03:26:22 +0700

    update handler http with cors

--- server/transports/http/handlers.go
+++ server/transports/http/handlers.go
@@ -2,11 +2,13 @@ package http
 
 import (
 	"github.com/bagasunix/ginclean/server/endpoints"
+	"github.com/bagasunix/ginclean/server/endpoints/middlewares"
 	"github.com/bagasunix/ginclean/server/transports/http/handlers"
 	"github.com/gin-gonic/gin"
 )
 
 func NewHttpHandler(r *gin.Engine, eps endpoints.Endpoints) *gin.Engine {
+	r.Use(middlewares.CORSMiddleware())
 
 	// Create an account group
 	handlers.MakeUserHandler(eps.RoleEndpoint, r.Group("/v1/role"))
