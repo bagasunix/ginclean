ee9e151de9cbd225dc9d3eaad79a7adf43533ecc
Aldino Pratama <aldinopratam15@gmail.com>
2022-11-03 22:40:36 +0700

    update route

--- server/transports/http/handlers/roles.go
+++ server/transports/http/handlers/roles.go
@@ -2,10 +2,16 @@ package handlers
 
 import (
 	"github.com/bagasunix/ginclean/server/endpoints"
+	"github.com/bagasunix/ginclean/server/endpoints/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
 func MakeUserHandler(eps endpoints.RoleEndpoint, rg *gin.RouterGroup) *gin.RouterGroup {
+	rg.Use(middlewares.CORSMiddleware())
 	rg.POST("", eps.CreateRole())
+	rg.GET("", eps.ListRole())
+	rg.GET("/:id", eps.ViewRole())
+	rg.PATCH("/:id", eps.UpdateRole())
+	rg.DELETE("/:id", eps.DeleteRole())
 	return rg
 }
