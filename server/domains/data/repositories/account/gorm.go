1b91537e4326ec287ee65629084a788fd1f946ae
Aldino Pratama <aldinopratam15@gmail.com>
2022-11-07 18:28:39 +0700

    update repo account

--- server/domains/data/repositories/account/gorm.go
+++ server/domains/data/repositories/account/gorm.go
@@ -3,17 +3,49 @@ package account
 import (
 	"context"
 
+	"github.com/bagasunix/ginclean/pkg/errors"
 	"github.com/bagasunix/ginclean/server/domains/data/models"
+	"github.com/gofrs/uuid"
+	"go.uber.org/zap"
 	"gorm.io/gorm"
 )
 
 type gormProvider struct {
-	db *gorm.DB
+	db   *gorm.DB
+	logs zap.Logger
+}
+
+// UpdateStatus implements AccountRepository
+func (g *gormProvider) UpdateStatus(ctx context.Context, id uuid.UUID, stat bool) error {
+	return errors.ErrSomethingWrong(g.logs, g.db.WithContext(ctx).Where("id = ?", id.String()).Update("isActive = ?", stat).Error)
+}
+
+// Delete implements AccountRepository
+func (g *gormProvider) Delete(ctx context.Context, id uuid.UUID) error {
+	return errors.ErrSomethingWrong(g.logs, g.db.WithContext(ctx).Delete(models.NewAccountBuilder().Build(), "id = ?", id.String()).Error)
+}
+
+// GetAll implements AccountRepository
+func (g *gormProvider) GetAll(ctx context.Context, limit int64) (result models.SliceResult[models.Account]) {
+	result.Error = errors.ErrRecordNotFound(g.logs, g.GetModelName(), g.db.WithContext(ctx).Limit(int(limit)).Find(&result.Value).Error)
+	return result
+}
+
+// GetByEmail implements AccountRepository
+func (g *gormProvider) GetByEmail(ctx context.Context, email string) (result models.SingleResult[*models.Account]) {
+	result.Error = errors.ErrRecordNotFound(g.logs, g.GetModelName(), g.db.WithContext(ctx).First(&result.Value, email).Error)
+	return result
+}
+
+// GetById implements AccountRepository
+func (g *gormProvider) GetById(ctx context.Context, id uuid.UUID) (result models.SingleResult[*models.Account]) {
+	result.Error = errors.ErrRecordNotFound(g.logs, g.GetModelName(), g.db.WithContext(ctx).First(&result.Value, id).Error)
+	return result
 }
 
 // Create implements AccountRepository
 func (g *gormProvider) Create(ctx context.Context, user *models.Account) error {
-	panic("unimplemented")
+	return errors.ErrDuplicateValue(g.logs, g.GetModelName(), g.db.WithContext(ctx).Create(user).Error)
 }
 
 // GetConnection implements AccountRepository
@@ -26,8 +58,9 @@ func (g *gormProvider) GetModelName() string {
 	return "Account"
 }
 
-func NewGorm(db *gorm.DB) AccountRepository {
+func NewGorm(logs zap.Logger, db *gorm.DB) AccountRepository {
 	g := new(gormProvider)
+	g.logs = logs
 	g.db = db
 	return g
 }
--- server/domains/data/repositories/account/repository.go
+++ server/domains/data/repositories/account/repository.go
@@ -5,13 +5,19 @@ import (
 
 	"github.com/bagasunix/ginclean/server/domains/data/models"
 	"github.com/bagasunix/ginclean/server/domains/data/repositories/base"
+	"github.com/gofrs/uuid"
 )
 
 type Command interface {
 	Create(ctx context.Context, user *models.Account) error
+	Delete(ctx context.Context, id uuid.UUID) error
+	UpdateStatus(ctx context.Context, id uuid.UUID, stat bool) error
 }
 
 type Query interface {
+	GetAll(ctx context.Context, limit int64) (result models.SliceResult[models.Account])
+	GetById(ctx context.Context, id uuid.UUID) (result models.SingleResult[*models.Account])
+	GetByEmail(ctx context.Context, email string) (result models.SingleResult[*models.Account])
 }
 
 type AccountRepository interface {
