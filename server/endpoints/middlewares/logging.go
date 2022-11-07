8e7889fdde3b8817437cb48e3e549b67804e1c36
Aldino Pratama <aldinopratam15@gmail.com>
2022-11-07 17:05:31 +0700

    add loging endpoint

--- server/endpoints/middlewares/logging.go
+++ server/endpoints/middlewares/logging.go
@@ -1,1 +1,20 @@
 package middlewares
+
+import (
+	"context"
+	"time"
+
+	"github.com/bagasunix/ginclean/server/endpoints"
+	"go.uber.org/zap"
+)
+
+func Logging(logs zap.Logger) endpoints.Middleware {
+	return func(e endpoints.Endpoint) endpoints.Endpoint {
+		return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+			defer func(begin time.Time) {
+				logs.Log(zap.DebugLevel, "Middleware Endpoints", zap.Any("transport_error", err), zap.Any("took", time.Since(begin)))
+			}(time.Now())
+			return e(ctx, request)
+		}
+	}
+}
