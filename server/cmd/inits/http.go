4f5be9a7184c354371f43c373d255289600b669b
Aldino Pratama <aldinopratam15@gmail.com>
2022-11-07 17:04:11 +0700

    add recoverywithzap

--- server/cmd/inits/http.go
+++ server/cmd/inits/http.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/bagasunix/ginclean/server/endpoints"
 	transportHttp "github.com/bagasunix/ginclean/server/transports/http"
+	ginzap "github.com/gin-contrib/zap"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 )
@@ -12,6 +13,7 @@ import (
 func InitHttpHandler(logs zap.Logger, endpoints endpoints.Endpoints) http.Handler {
 	// r := gin.Default()
 	r := gin.New()
+	r.Use(ginzap.RecoveryWithZap(&logs, true))
 
-	return transportHttp.NewHttpHandler(logs, r, endpoints)
+	return transportHttp.NewHttpHandler(r, endpoints)
 }
