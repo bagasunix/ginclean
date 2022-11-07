aeae255bb8a6223b89b61325c22efe6771c8b233
Aldino Pratama <aldinopratam15@gmail.com>
2022-11-07 21:42:39 +0700

    add getaccount in service repo

--- server/domains/data/repositories/repositories.go
+++ server/domains/data/repositories/repositories.go
@@ -1,6 +1,7 @@
 package repositories
 
 import (
+	"github.com/bagasunix/ginclean/server/domains/data/repositories/account"
 	"github.com/bagasunix/ginclean/server/domains/data/repositories/role"
 	"go.uber.org/zap"
 	"gorm.io/gorm"
@@ -8,10 +9,17 @@ import (
 
 type Repositories interface {
 	GetRole() role.Repository
+	GetAccount() account.Repository
 }
 
 type repo struct {
-	role role.Repository
+	role    role.Repository
+	account account.Repository
+}
+
+// GetAccount implements Repositories
+func (r *repo) GetAccount() account.Repository {
+	return r.account
 }
 
 // GetRole implements Repositories
@@ -22,5 +30,6 @@ func (r *repo) GetRole() role.Repository {
 func New(logs zap.Logger, db *gorm.DB) Repositories {
 	rs := new(repo)
 	rs.role = role.NewGorm(logs, db)
+	rs.account = account.NewGorm(logs, db)
 	return rs
 }
