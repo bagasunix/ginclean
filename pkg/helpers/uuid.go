e78b704e680efc2147aa440c9fe991f4d0ef8820
Aldino Pratama <aldinopratam15@gmail.com>
2022-11-07 21:44:28 +0700

    update generate uuid with logs

--- pkg/helpers/uuid.go
+++ pkg/helpers/uuid.go
@@ -3,15 +3,16 @@ package helpers
 import (
 	"github.com/bagasunix/ginclean/pkg/errors"
 	"github.com/gofrs/uuid"
+	"go.uber.org/zap"
 )
 
-func GenerateUUIDV1() uuid.UUID {
+func GenerateUUIDV1(logs zap.Logger) uuid.UUID {
 	id, err := uuid.NewV1()
 	errors.HandlerReturnedVoid(err, "uuid", "generator")
 	return id
 }
 
-func GenerateUUIDV4() uuid.UUID {
+func GenerateUUIDV4(logs zap.Logger) uuid.UUID {
 	id, err := uuid.NewV4()
 	errors.HandlerReturnedVoid(err, "uuid", "generator")
 	return id
