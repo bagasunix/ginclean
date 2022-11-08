eeeae7be1788db5e38b8c9dfc5d709ae8bec4019
Aldino Pratama <aldinopratam15@gmail.com>
2022-11-08 15:37:16 +0700

    add endpoint delete account

--- server/transports/http/handlers/user.go
+++ server/transports/http/handlers/user.go
@@ -8,5 +8,6 @@ import (
 func MakeUserHandler(eps endpoints.UserEndpoint, rg *gin.RouterGroup) *gin.RouterGroup {
 	rg.POST("", eps.CreateUser())
 	rg.GET("", eps.ListAccount())
+	rg.DELETE("/:id", eps.DeleteAccount())
 	return rg
 }
