a4ceb5746469f1c5950d6424cd48e2d470f56938
Aldino Pratama <aldinopratam15@gmail.com>
2022-11-08 18:46:53 +0700

    update repo

--- server/domains/data/repositories/account/repository.go
+++ server/domains/data/repositories/account/repository.go
@@ -11,7 +11,7 @@ import (
 type Command interface {
 	Create(ctx context.Context, user *models.Account) error
 	Delete(ctx context.Context, id uuid.UUID) error
-	UpdateStatus(ctx context.Context, id uuid.UUID, stat bool) error
+	UpdateStatus(ctx context.Context, model *models.Account) error
 }
 
 type Query interface {
