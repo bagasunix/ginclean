f7293ad0837ee4485d5fe942c996997b7d227f74
Aldino Pratama <aldinopratam15@gmail.com>
2022-11-08 10:27:49 +0700

    update init http

--- server/cmd/inits/http.go
+++ server/cmd/inits/http.go
@@ -5,15 +5,12 @@ import (
 
 	"github.com/bagasunix/ginclean/server/endpoints"
 	transportHttp "github.com/bagasunix/ginclean/server/transports/http"
-	ginzap "github.com/gin-contrib/zap"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 )
 
-func InitHttpHandler(logs zap.Logger, endpoints endpoints.Endpoints) http.Handler {
-	// r := gin.Default()
+func InitHttpHandler(logs *zap.Logger, endpoints endpoints.Endpoints) http.Handler {
 	r := gin.New()
-	r.Use(ginzap.RecoveryWithZap(&logs, true))
 
-	return transportHttp.NewHttpHandler(r, endpoints)
+	return transportHttp.NewHttpHandler(logs, r, endpoints)
 }
