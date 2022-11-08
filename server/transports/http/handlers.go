28b0bfe862c7486fddb328a5d232422739bf2f37
Aldino Pratama <aldinopratam15@gmail.com>
2022-11-08 10:28:14 +0700

    update handler endpoint

--- server/transports/http/handlers.go
+++ server/transports/http/handlers.go
@@ -4,11 +4,14 @@ import (
 	"github.com/bagasunix/ginclean/server/endpoints"
 	"github.com/bagasunix/ginclean/server/endpoints/middlewares"
 	"github.com/bagasunix/ginclean/server/transports/http/handlers"
+	ginzap "github.com/gin-contrib/zap"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
-func NewHttpHandler(r *gin.Engine, eps endpoints.Endpoints) *gin.Engine {
+func NewHttpHandler(logs *zap.Logger, r *gin.Engine, eps endpoints.Endpoints) *gin.Engine {
 	r.Use(middlewares.CORSMiddleware())
+	r.Use(ginzap.RecoveryWithZap(logs, true))
 
 	// Create an account group
 	handlers.MakeRoleHandler(eps.RoleEndpoint, r.Group("/v1/role"))
