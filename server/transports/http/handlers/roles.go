87184a841a30e4257fac5cdd8b64f141020edc25
Aldino Pratama <aldinopratam15@gmail.com>
2022-11-11 00:06:32 +0700

    add mdw auth

--- server/transports/http/handlers/roles.go
+++ server/transports/http/handlers/roles.go
@@ -2,10 +2,12 @@ package handlers
 
 import (
 	"github.com/bagasunix/ginclean/server/endpoints"
+	"github.com/bagasunix/ginclean/server/endpoints/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
 func MakeRoleHandler(eps endpoints.RoleEndpoint, rg *gin.RouterGroup) *gin.RouterGroup {
+	rg.Use(middlewares.Auth())
 	rg.POST("", eps.CreateRole())
 	rg.POST("/create-multiple-role", eps.CreateMultiRole())
 	rg.GET("", eps.ListRole())
