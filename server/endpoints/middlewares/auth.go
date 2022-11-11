6b27844bddf841bc49a7a5c8525e7309e5afcef6
Aldino Pratama <aldinopratam15@gmail.com>
2022-11-11 11:40:46 +0700

    update midw auth

--- server/endpoints/middlewares/auth.go
+++ server/endpoints/middlewares/auth.go
@@ -1,28 +1,49 @@
 package middlewares
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/bagasunix/ginclean/pkg/jwt"
 	"github.com/gin-gonic/gin"
 )
 
+const (
+	authorizationHeaderKey  = "authorization"
+	authorizationTypeBearer = "bearer"
+	authorizationPayloadKey = "authorization_payload"
+)
+
 func Auth() gin.HandlerFunc {
 	return func(context *gin.Context) {
-		tokenString := context.GetHeader("Authorization")
-		tokenString = strings.Replace(tokenString, "Bearer ", "", 1)
+		tokenString := context.GetHeader(authorizationHeaderKey)
 		if tokenString == "" {
 			context.JSON(401, gin.H{"error": "You are not logged in! Please log in to get access."})
 			context.Abort()
 			return
 		}
 
-		err := jwt.ValidateToken(tokenString)
+		fields := strings.Fields(tokenString)
+		if len(fields) < 2 {
+			context.JSON(401, gin.H{"error": "invalid authorization header format"})
+			context.Abort()
+			return
+		}
+
+		authorizationType := strings.ToLower(fields[0])
+		if authorizationType != authorizationTypeBearer {
+			context.JSON(401, gin.H{"error": fmt.Errorf("unsupported authorization type %s", authorizationType)})
+			context.Abort()
+			return
+		}
+
+		claims, err := jwt.ValidateToken(fields[1])
 		if err != nil {
 			context.JSON(401, gin.H{"error": err.Error()})
 			context.Abort()
 			return
 		}
+		context.Set(authorizationPayloadKey, *claims.User)
 		context.Next()
 	}
 }
