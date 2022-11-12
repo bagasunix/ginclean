9f90a88188d40018b6038ad49f34f0125727d49c
Aldino Pratama <aldinopratam15@gmail.com>
2022-11-12 21:26:10 +0700

    update migration table

--- server/cmd/inits/migration.go
+++ server/cmd/inits/migration.go
@@ -10,7 +10,13 @@ import (
 func GetTables() (tables []interface{}) {
 	tables = append(tables, models.NewAccountBuilder().Build())
 	tables = append(tables, models.NewRoleBuilder().Build())
+	tables = append(tables, models.NewUserBuilder().Build())
 	tables = append(tables, models.NewRefershTokenBuilder().Build())
+	tables = append(tables, models.NewCountryBuilder().Build())
+	tables = append(tables, models.NewProvinceBuilder().Build())
+	tables = append(tables, models.NewCityBuilder().Build())
+	tables = append(tables, models.NewSubDistrictBuilder().Build())
+	tables = append(tables, models.NewVillageBuilder().Build())
 	return tables
 }
 func Migrate(logs zap.Logger, db *gorm.DB) {
