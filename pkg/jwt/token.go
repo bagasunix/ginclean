0089b28212f022a78f836147796fd3d5cbbe099f
Aldino Pratama <aldinopratam15@gmail.com>
2022-11-13 12:46:11 +0700

    create valid token key refresh

--- pkg/jwt/token.go
+++ pkg/jwt/token.go
@@ -1,11 +1,11 @@
 package jwt
 
 import (
-	"errors"
 	"fmt"
 	"time"
 
 	envs "github.com/bagasunix/ginclean/pkg/env"
+	"github.com/bagasunix/ginclean/pkg/errors"
 	"github.com/golang-jwt/jwt/v4"
 )
 
@@ -18,7 +18,8 @@ func ValidateToken(signedToken string) (claims *Claims, err error) {
 	conf, _ := envs.LoadEnv()
 	t, err := jwt.ParseWithClaims(signedToken, &Claims{}, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, errors.New(fmt.Sprintf("unexpected signing method: %v", token.Header["alg"]))
+			err = errors.CustomError(fmt.Sprintf("unexpected signing method: %v", token.Header["alg"]))
+			return nil, err
 		}
 		return []byte(conf.JwtSecret), nil
 	})
@@ -29,12 +30,40 @@ func ValidateToken(signedToken string) (claims *Claims, err error) {
 
 	claims, ok := t.Claims.(*Claims)
 	if !ok {
-		err = errors.New("couldn't parse claims")
+		err = errors.CustomError("couldn't parse claims")
 		return
 	}
 
 	if claims.ExpiresAt < time.Now().Local().Unix() {
-		err = errors.New("token expired")
+		err = errors.CustomError("token expired")
+		return
+	}
+
+	return claims, err
+}
+
+func ValidateRefreshToken(signedToken string) (claims *Claims, err error) {
+	conf, _ := envs.LoadEnv()
+	t, err := jwt.ParseWithClaims(signedToken, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			err = errors.CustomError(fmt.Sprintf("unexpected signing method: %v", token.Header["alg"]))
+			return nil, err
+		}
+		return []byte(conf.JwtSecretRefresh), nil
+	})
+
+	if err != nil {
+		return
+	}
+
+	claims, ok := t.Claims.(*Claims)
+	if !ok {
+		err = errors.CustomError("couldn't parse claims")
+		return
+	}
+
+	if claims.ExpiresAt < time.Now().Local().Unix() {
+		err = errors.CustomError("token expired")
 		return
 	}
 
