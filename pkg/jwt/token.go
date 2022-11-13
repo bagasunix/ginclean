08e3d473df207e3bb11a1d024b32ac84f06011ee
Aldino Pratama <aldinopratam15@gmail.com>
2022-11-14 00:13:28 +0700

    update validate refresh token

--- pkg/jwt/token.go
+++ pkg/jwt/token.go
@@ -7,6 +7,7 @@ import (
 	envs "github.com/bagasunix/ginclean/pkg/env"
 	"github.com/bagasunix/ginclean/pkg/errors"
 	"github.com/golang-jwt/jwt/v4"
+	"go.uber.org/zap"
 )
 
 func GenerateToken(jwtKey string, claims Claims) (token string, err error) {
@@ -42,29 +43,18 @@ func ValidateToken(signedToken string) (claims *Claims, err error) {
 	return claims, err
 }
 
-func ValidateRefreshToken(signedToken string) (claims *Claims, err error) {
+func ValidateRefreshToken(logs zap.Logger, signedToken string) (claims *Claims, err error) {
 	conf, _ := envs.LoadEnv()
-	t, err := jwt.ParseWithClaims(signedToken, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+	t, _ := jwt.ParseWithClaims(signedToken, &Claims{}, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			err = errors.CustomError(fmt.Sprintf("unexpected signing method: %v", token.Header["alg"]))
-			return nil, err
+			return claims, errors.ErrSomethingWrong(logs, errors.CustomError(fmt.Sprintf("unexpected signing method: %v", token.Header["alg"])))
 		}
 		return []byte(conf.JwtSecretRefresh), nil
 	})
 
-	if err != nil {
-		return
-	}
-
 	claims, ok := t.Claims.(*Claims)
 	if !ok {
-		err = errors.CustomError("couldn't parse claims")
-		return
-	}
-
-	if claims.ExpiresAt < time.Now().Local().Unix() {
-		err = errors.CustomError("token expired")
-		return
+		return claims, errors.ErrSomethingWrong(logs, errors.CustomError("couldn't parse claims"))
 	}
 
 	return claims, err
