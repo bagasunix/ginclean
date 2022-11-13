80c5823e4d8726bebde223ef10ec02a122061fec
Aldino Pratama <aldinopratam15@gmail.com>
2022-11-14 00:16:05 +0700

    add handler auth

--- server/transports/http/handlers.go
+++ server/transports/http/handlers.go
@@ -11,5 +11,6 @@ func NewHttpHandler(r *gin.Engine, eps endpoints.Endpoints) *gin.Engine {
 	// Create an account group
 	handlers.MakeRoleHandler(eps.RoleEndpoint, r.Group("/v1/role"))
 	handlers.MakeUserHandler(eps.UserEndpoint, r.Group("/v1/user"))
+	handlers.MakeAuthHandler(eps.RefreshToken, r.Group("/v1/auth"))
 	return r
 }
---
+++ server/transports/http/handlers/auth.go
@@ -0,0 +1,11 @@
+package handlers
+
+import (
+	"github.com/bagasunix/ginclean/server/endpoints"
+	"github.com/gin-gonic/gin"
+)
+
+func MakeAuthHandler(eps endpoints.AuthEndpoint, rg *gin.RouterGroup) *gin.RouterGroup {
+	rg.POST("/refresh_token", eps.RefreshToken())
+	return rg
+}
