3738c637ca308c1262ea3b018ed57fabbb42de3e
Aldino Pratama <aldinopratam15@gmail.com>
2022-11-14 12:11:25 +0700

    update logger

--- server/cmd/inits/logger.go
+++ server/cmd/inits/logger.go
@@ -1,22 +1,35 @@
 package inits
 
 import (
+	"fmt"
 	"os"
+	"path"
+	"strconv"
 	"sync"
 
+	envs "github.com/bagasunix/ginclean/pkg/env"
+	"github.com/bagasunix/ginclean/pkg/helpers"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 var lock = &sync.Mutex{}
 var Loggers *zap.Logger
 
-func InitLogger() *zap.Logger {
+func InitLogger(config *envs.Configs) *zap.Logger {
+	fmt.Println(config.LogPath)
+	writerSyncer := getLogWriter(config)
 	var core zapcore.Core
 
-	core = zapcore.NewTee(
-		zapcore.NewCore(getConsoleEncoder(), zapcore.AddSync(os.Stdout), zapcore.DebugLevel),
-	)
+	if os.Getenv("ENV") == "dev" {
+		core = zapcore.NewTee(
+			zapcore.NewCore(getConsoleEncoder(), zapcore.AddSync(os.Stdout), zapcore.DebugLevel),
+			zapcore.NewCore(getFileEncoder(), writerSyncer, zapcore.DebugLevel),
+		)
+	} else {
+		core = zapcore.NewCore(getFileEncoder(), writerSyncer, zapcore.DebugLevel)
+	}
 
 	Loggers = zap.New(core, zap.AddCaller())
 
@@ -28,4 +41,31 @@ func getConsoleEncoder() zapcore.Encoder {
 	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
 	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
 	return zapcore.NewConsoleEncoder(encoderConfig)
+}
+
+func getFileEncoder() zapcore.Encoder {
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	encoderConfig.StacktraceKey = zapcore.DPanicLevel.String()
+	return zapcore.NewJSONEncoder(encoderConfig)
+}
+
+func getLogWriter(config *envs.Configs) zapcore.WriteSyncer {
+
+	logFilePath := config.LogPath
+	logFileName := config.LogName + "-" + helpers.DateFormatEN() + ".log"
+	logFileMaxSize, _ := strconv.Atoi(config.LogMaxSize)
+	logFileMaxBackups, _ := strconv.Atoi(config.LogMaxBackup)
+	logFileMaxAge, _ := strconv.Atoi(config.LogMaxAge)
+	logFile := path.Join(logFilePath, logFileName)
+	lumberJackLogger := &lumberjack.Logger{
+		Filename:   logFile,
+		MaxSize:    logFileMaxSize,
+		MaxBackups: logFileMaxBackups,
+		MaxAge:     logFileMaxAge,
+		Compress:   true,
+		LocalTime:  true,
+	}
+	return zapcore.AddSync(lumberJackLogger)
 }
